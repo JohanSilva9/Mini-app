34247b8d327848260083b394592b16fdda7e0f48
Sala D6 <CSalaD6@uniboyaca.edu.co>
2025-09-16 18:14:02 -0500

    Primer commit

---
+++ App_Ionic/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicModule, AlertController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { PRODUCTS } from './data/products';
+import type { Product } from './models/product';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [IonicModule, CommonModule, ProductCardComponent],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  products: Product[] = PRODUCTS;
+
+  constructor(private alertCtrl: AlertController) {}
+
+  async mostrarDetalle(product: Product) {
+  const alert = await this.alertCtrl.create({
+    header: product.name,
+    message: `Precio: $${product.price}\n${product.description ?? ''}`,
+    buttons: ['Cerrar']
+  });
+  await alert.present();
+}
+}
